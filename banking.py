d787edb182bea31a7f0f06c57fb6d8721cf40fa1
Maher Qarni <qarnimaher@gmail.com>
2025-09-22 16:54:30 +0300

    today's work

--- banking.py
+++ banking.py
@@ -3,183 +3,195 @@ import csv
 import os
 
 fieldnames = ["id","first_name","last_name","password","checking","savings","active","overdraft_count"]
-options = ['1', '2' , '3']
+options = ['1', '2' , '3','4','5']
+options_y_n =['Y' , 'N']
 
 
 class User:
-    def __init__(self , id,first_name, last_name, password , accounts = [None ,None], active = True , overdraft_count=0):
+    def __init__(self, id, first_name, last_name, password , checking="False", savings="False", active = True , overdraft_count=0):
         self.id = id
         self.first_name = first_name
-        self.last_name = last_name
-        self.password = password
-        self.accounts = accounts 
-        self.active = active
+        self.last_name  = last_name
+        self.password   = password
+        self.checking   = False if checking == "False" or checking == False else int(checking)
+        self.savings    = False if savings == "False" or savings == False else int(savings)
+        self.active     = False if active == "False" else True
         self.overdraft_count = overdraft_count     
 
-
+    # def get
     def __str__(self):
-        return f"{self.id} , {self.first_name} {self.last_name} , {self.password} , {self.accounts} , {self.active} , {self.overdraft_count}"
+        return f"{self.id} , {self.first_name} {self.last_name} , {self.password} , {self.checking}, {self.savings} , {self.active} , {self.overdraft_count}"
     
-class Account:
-    def __init__(self, owner,balance = 0):
-        self.owner = owner
-        self.balance = balance
-class Checking(Account):
-    def __init__(self, owner, balance=0):
-        super().__init__(owner, balance)
+# class Account:
+#     def __init__(self, owner,balance = 0):
+#         self.owner = owner
+#         self.balance = balance
 
-class Savings(Account):
-    def __init__(self, owner, balance=0):
-        super().__init__(owner, balance)
+
+# class Checking(Account):
+#     def __init__(self, owner, balance=0):
+#         super().__init__(owner, balance)
+
+# class Savings(Account):
+#     def __init__(self, owner, balance=0):
+#         super().__init__(owner, balance)
 
 class Bank:
+    
     def __init__(self):
         self.users = {}
-        self.ids= 100013
         self.load_users()
-
-
-    def prepare_user(self, **Kkwargs):
-        info = Kkwargs
-        user = User(info['id'] , info['first_name'], info['last_name'], info['password'] , [info['checking'] , info ['savings']] , info['active'] , info ['overdraft_count']) 
-        return user
-
+        self.ids =  100000 + len(self.users) + 1
+        self.user = None
     
     def load_users(self):
         try: 
             with open("bank.csv", "r") as file:
                 contents = csv.DictReader(file)
                 for row in contents:
-                    user = self.prepare_user(**row)
-                    self.users[user.id] = user 
+                    self.users[row["id"]] = User(row['id'] , row['first_name'], row['last_name'], row['password'] , row['checking'] , row ['savings'], row['active'] , row ['overdraft_count'])  
         except csv.Error as e:
             print(e)
 
     def sign_in(self):
         print()
         print("="*40)
-        print("       Sign IN  ")
-        print()
+        print("       Sign IN  \n\n")
 
-        user_id = input('Enter the ID: ')
-        print()
+        user_id = None
+        while not user_id or user_id not in self.users:
+            user_id = input('Enter the ID: \n')
+            if user_id not in self.users:
+                print("Account ID doesn't exists! \n")
+        
+        print(f"Hi {self.users[user_id].first_name} {self.users[user_id].last_name} \n")
 
-        while user_id not in self.users :
-            print("Account ID doesn't exists!")
-            print()
-            user_id = input('Enter the ID: ')
-        print()
-        print(f"Hi {self.users[user_id]['first_name']} {self.users[user_id]['last_name']}")
-        print()
-
-        user_pass = input("Enter Password: ")
-        print()
-
-
-
-        while user_pass != self.users[user_id]['password'] :
-            print("Invalid Password!, Please Try Agin")
-            print()
+        user_pass = None
+        while not user_pass or user_pass != self.users[user_id].password:
             user_pass = input('Enter Password: ')
+            if user_pass != self.users[user_id].password:
+                print("Invalid Password!, Please Try Agin. \n\n")
 
-        print('Successful! ')
-        print()
-        pprint.pprint(self.users[user_id], sort_dicts=False)
+        print('Successful! \n')
+        self.user = self.users[user_id]
+
     
     def create_user(self):
-        print()
         print("="*40)
-        print("       Create User  ")
-        print()
+        print("       Create User  \n")
         user_first_name = input('Enter First Name: ')
         print()
         user_last_name = input('Enter Last Name: ')
         print()
         user_password = input('Enter Password: ')
         print()
-
         user = User(self.ids , user_first_name, user_last_name,user_password)
         self.users[user.id] = user
         self.ids +=1
-        print('User is created successfully')
-        for idx , user in self.users.items():
-            print(user)
-        print(self.ids)
-
-
-        # write to csv 
-
-
+        self.user = user
         
+        try:
+            new_row = {'id': user.id,
+                        'first_name': user.first_name,
+                        'last_name': user.last_name,
+                        'password' : f'{user.password}',
+                        'checking': user.checking,
+                        'savings' : user.savings,
+                        'active' : user.active,
+                        'overdraft_count' : user.overdraft_count}
+            
+            with open("bank.csv", "a+") as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writerow(new_row)
+        except csv.Error as e:
+            print(e)
+
+        print('User Created Successfully')
 
 
-# 3.0 Check CSV File Exists (otherwise error thrown!)
-# 3.1 Set Headers in the CSVFile 
-# 3.2 SEED DATA TO CSV
-# 3.3 EXAMPLE: writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-# 3.4 "w" option will allow writing, but NOT appending...
-# if not os.path.exists("./doctor_who.csv"):
-#     with open("./doctor_who.csv", 'w', newline='') as csvfile:
-#         try:
-#             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#             writer.writeheader()
-#             for row in doctor_who_info:
-#                 writer.writerow(row)
-#         except csv.Error as e:
-#             print(e)
+    def user_menu (self):
 
+        print("="*40)
+        print(f"       Hi {self.user.first_name} {self.user.last_name}!  ")
+        print("="*40)
+        print("\n1) Create Account")
+        print("2) Withdraw")
+        print("3) Deposit")
+        print("4) Tansfer")
+        print("5) Log Out")
+        print("="*40)
 
-# 4.0 If Exists - ReadFile / Rows:
+        user_option = None
+        while not user_option or user_option not in options:
+            user_option = input("Pick an option: ")
+            if user_option not in options:
+                print('Invalid Option, try again')
 
+        match user_option:
+            case '1':
+                self.create_account()
+            case '2':
+                pass
+            case '3':
+                pass
+            case '4':
+                pass
+            case '5':
+                return False
+        return True
 
-# def sign_in():
-#     print()
-#     print("="*40)
-#     print("       Sign IN  ")
-#     print()
-
-#     user_id = input('Enter the ID: ')
-#     print()
-    
-#     while user_id not in accounts_for_now :
-#         print("Account ID doesn't exists!")
-#         print()
-#         user_id = input('Enter the ID: ')
-#     print()
-#     print(f"Hi {accounts_for_now[user_id]['first_name']} {accounts_for_now[user_id]['last_name']}")
-#     print()
-
-#     user_pass = input("Enter Password: ")
-#     print()
-
-
-
-#     while user_pass != accounts_for_now[user_id]['password'] :
-#         print("Invalid Password!, Please Try Agin")
-#         print()
-#         user_pass = input('Enter Password: ')
-
-#     print('Successful! ')
-#     print()
-#     pprint.pprint(accounts_for_now[user_id], sort_dicts=False)
-
-# def create_account():
-#     print()
-#     print("="*40)
-#     print("       Sign IN  ")
-#     print()
-
-#     # user_id = input('Enter the ID: ')
-#     # for idx ,user in self.users.items():
-
+    def create_account(self):
+        if type(self.user.checking) != bool and type(self.user.savings) != bool:
+            print("U have checking and savings accounts already, you can't create more \n" )
+            return
         
+        print(f"\n       Pick checking or savings   ")
+        print("="*40)
+        print("1) Checking")
+        print("2) Savings")
+        print("="*40)
+
+        user_option = None
+        while not user_option or user_option not in options[:2]:
+            user_option = input("Pick an option: ")
+            if user_option not in options[:2]:
+                print('Invalid Option, try again')
+        
+        if user_option == '1':
+            if type(self.user.checking) == bool:
+                self.user.checking = 0
+                print("Checking account created successfully") 
+            else:
+                print ("you already have checking account")
+        if user_option == '2':
+            if type(self.user.savings) == bool:
+                self.user.savings = 0
+                print("Savings account created successfully") 
+            else:
+                print ("you already have savings account")
+        
+        self.update_csv()
     
+    def update_csv(self):
+        if  os.path.exists("bank.csv"):
+            with open("bank.csv", 'w', newline='') as csvfile:
+                try:
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                    for key, row in self.users.items():
+                        writer.writerow(row.__dict__)
+                except csv.Error as e:
+                    print(e)
+
+        self.load_users()
+
+
 
 def init():
     bank = Bank()
 
-    for idx ,user in bank.users.items():
-        print(user)
+    # for idx ,user in bank.users.items():
+    #     print(user)
     print("="*40)
     print("       Welcome to Maher Bank!  ")
     print("="*40)
@@ -192,7 +204,7 @@ def init():
 
     user_option = input('Pick an option: ')
 
-    while user_option not in options : 
+    while user_option not in options:
         print('Invalid option')
         user_option = input('Pick an option: ')
     
@@ -200,6 +212,11 @@ def init():
         bank.sign_in()
     if user_option == '2':
         bank.create_user()
+
+    if bank.user:
+        while bank.user_menu():
+            bank.user_menu()
+
 
 
 init()
